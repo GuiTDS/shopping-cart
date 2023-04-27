b94bc6edc255ddacd9d48211c4875b860c769ed1
Guilherme Sanches <guitdsanches@gmail.com>
2023-04-27 10:48:33 -0300

    adding payment context

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,16 +1,22 @@
 import { Button, Snackbar, InputLabel } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Container, Voltar, TotalContainer, PagamentoContainer } from './styles';
 import { useCarrinhoContext } from 'common/context/Carrinho';
 import Produto from 'components/Produto';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { PagamentoContext } from 'common/context/Pagamento';
 
 function Carrinho() {
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const { carrinho } = useCarrinhoContext();
+  const history = useHistory();
+  const { formaPagamento } = useContext(PagamentoContext);
+
   return (
     <Container>
-      <Voltar />
+      <Voltar onClick={() => history.goBack()}/>
+      {formaPagamento.nome}
       <h2>
         Carrinho
       </h2>
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Feira from "pages/Feira";
 import Login from "pages/Login";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { CarrinhoProvider } from "common/context/Carrinho";
+import { PagamentoProvider } from "common/context/Pagamento";
 
 function Router() {
   return (
@@ -17,9 +18,11 @@ function Router() {
             <Route path="/feira">
               <Feira />
             </Route>
-            <Route path="/carrinho">
-              <Carrinho />
-            </Route>
+            <PagamentoProvider>
+              <Route path="/carrinho">
+                <Carrinho />
+              </Route>
+            </PagamentoProvider>
           </CarrinhoProvider>
         </UsuarioProvider>
 
