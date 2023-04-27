8eb4ea0035881558336949b97eb8d3c91a3b6fa5
Guilherme Sanches <guitdsanches@gmail.com>
2023-04-27 11:52:08 -0300

    creating a personalized hook for payment

--- src/common/context/Pagamento.js
+++ src/common/context/Pagamento.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 
 export const PagamentoContext = createContext();
@@ -33,5 +33,21 @@ export const PagamentoProvider = ({ children }) => {
             {children}
         </PagamentoContext.Provider>
      );
+}
+
+export const usePagamentoContext = () => {
+    const { tiposPagamento, formaPagamento, setFormaPagamento } = useContext(PagamentoContext);
+
+    function mudarFormaPagamento(id) {
+        const pagamentoAtual = tiposPagamento.find(pagamento => pagamento.id === id);
+
+        setFormaPagamento(pagamentoAtual);
+    }
+
+    return {
+        tiposPagamento,
+        formaPagamento,
+        mudarFormaPagamento
+    }
 }
  
--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,22 +1,21 @@
-import { Button, Snackbar, InputLabel } from '@material-ui/core';
+import { Button, Snackbar, InputLabel, Select, MenuItem } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 import { Container, Voltar, TotalContainer, PagamentoContainer } from './styles';
 import { useCarrinhoContext } from 'common/context/Carrinho';
 import Produto from 'components/Produto';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import { PagamentoContext } from 'common/context/Pagamento';
+import { usePagamentoContext } from 'common/context/Pagamento';
 
 function Carrinho() {
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const { carrinho } = useCarrinhoContext();
   const history = useHistory();
-  const { formaPagamento } = useContext(PagamentoContext);
+  const { tiposPagamento, formaPagamento, mudarFormaPagamento } = usePagamentoContext();
 
   return (
     <Container>
       <Voltar onClick={() => history.goBack()}/>
-      {formaPagamento.nome}
       <h2>
         Carrinho
       </h2>
@@ -29,6 +28,18 @@ function Carrinho() {
       })}
       <PagamentoContainer>
         <InputLabel> Forma de Pagamento </InputLabel>
+        <Select
+        value={formaPagamento.id}
+        onChange={(event) => mudarFormaPagamento(event.target.value)}
+        >
+          {tiposPagamento.map(pagamento => {
+            return(
+              <MenuItem value={pagamento.id} key={pagamento.id}>
+                {pagamento.nome}
+              </MenuItem>
+            )
+          })}
+        </Select>
       </PagamentoContainer>
       <TotalContainer>
         <div>
